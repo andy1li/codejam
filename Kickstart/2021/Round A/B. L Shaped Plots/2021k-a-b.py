5017cd3564d9152146dfac3c3b5ea737d97c358e
Andy Li <li.chenxing@gmail.com>
2021-03-21 18:07:59 +0800

    Refactor Kickstart/2021/A/B

--- Kickstart/2021/Round A/B. L Shaped Plots/2021k-a-b.py
+++ Kickstart/2021/Round A/B. L Shaped Plots/2021k-a-b.py
@@ -2,55 +2,48 @@
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c509
 
 from collections import defaultdict
+from itertools import groupby
 Ints = lambda: [*map(int, input().split())]
 
 #------------------------------------------------------------------------------#
 
-def intevels(r, c, grid):
-    rows, cols = defaultdict(list), defaultdict(list)
-    for i in range(r):
-        start = -1
-        for j in range(c):
-            if grid[i][j]:
-                if start == -1: start = j
-            else:
-                if start != -1: 
-                    rows[i].append((start, j))
-                    start = -1
-        if start != -1: rows[i].append((start, c))
-
-    for i in range(c):
-        start = -1
-        for j in range(r):
-            if grid[j][i]: 
-                if start == -1: start = j
-            else:
-                if start != -1: 
-                    cols[i].append((start, j))
-                    start = -1
-        if start != -1: cols[i].append((start, r))
-
+def condense(r, c, grid):
+    def intervals(line, i=0):
+        for key, g in groupby(line):
+            l = len(list(g))
+            if key and l > 1: yield i, i+l
+            i += l
+            
+    rows = [ list(intervals(row)) for row in grid ]
+    cols = [ list(intervals(grid[i][j] for i in range(r))) for j in range(c) ]
     return rows, cols
 
 def junctions(rows, cols):
-    for i, row in rows.items():
-        for hor in row:
-            if hor[1] - hor[0] < 2: continue
-            for j in range(*hor):
-                ver = next(ver for ver in cols[j] if ver[0]<=i<ver[1])
-                if ver[1] - ver[0] < 2: continue
-                # print(i, j, hor, ver)
-                yield i-ver[0]+1, hor[1]-j, ver[1]-i, j-hor[0]+1
+    def junction(w, e, i, j):
+        try:
+            n, s = next((n, s) for n, s in cols[j] if n<=i<s)
+            # print(i, j, (w, e), (n, s))
+            return i-n+1, e-j, s-i, j-w+1 # N, E, S, W
+        except StopIteration: return
 
-def count(nesw):
-    nesw += nesw[0],
+    yield from (
+        junction(w, e, i, j)
+        for i, row in enumerate(rows)
+        for w, e in row
+        for j in range(w, e)
+    )
+
+def count(junction):
+    if not junction: return 0
+    junction += junction[0],
     return sum(
-        max(0, min(a-1, b//2-1)) + max(0, min(b-1, a//2-1))
-        for a, b in zip(nesw, nesw[1:])
+        max(0, min(a, b//2)-1) 
+      + max(0, min(b, a//2)-1)
+        for a, b in zip(junction, junction[1:])
     )
 
 def solve(r, c, grid):
-    rows, cols = intevels(r, c, grid)
+    rows, cols = condense(r, c, grid)
     return sum(map(count, junctions(rows, cols)))
 
 #------------------------------------------------------------------------------#
