c8fd96d7aacb53a7c6bd3abcaaf4c7f27214694e
Andy Li <1450947+andy1li@users.noreply.github.com>
2022-05-23 09:37:04 +0800

    Kickstart/2022/C/B

--- Kickstart/2022/Round C/B. Range Partition/2022k-c-b.py
+++ Kickstart/2022/Round C/B. Range Partition/2022k-c-b.py
@@ -1,15 +1,32 @@
 # 2022 Kickstart Round C - B. Range Partition
 # https://codingcompetitions.withgoogle.com/kickstart/round/00000000008cb4d1/0000000000b20deb
 
-import sys; sys.stdin = open('B. Range Partition/sample.in', 'r')
+import sys; sys.stdin = open('B. Range Partition/ts2_input.txt', 'r')
+sys.setrecursionlimit(10**9)
 
 Int = lambda: int(input())
 Ints = lambda: [*map(int, input().split())]
 
 #------------------------------------------------------------------------------#
 
+def partition(n, p_sum):
+    if (p_sum == 0 or n == 0): return []
+    return (
+        partition(p_sum, p_sum) if n > p_sum else 
+        [n] + partition(n-1, p_sum-n)
+    )
+
 def solve(n, x, y):
-    print(n, x, y)
+    sum_n = n * (n + 1) // 2
+    if sum_n % (x + y) != 0: return 'IMPOSSIBLE'
+
+    a = sum_n * x // (x + y)
+    p_a = partition(n, a)
+
+    return f'''POSSIBLE
+{len(p_a)}
+{' '.join(map(str, p_a))}'''
+
 
 #------------------------------------------------------------------------------#
 
